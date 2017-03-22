574834545d48a95a0a75b27cc27762650ed7668a
Prasanna Natarajan <prasanna2431997@gmail.com>
2017-03-23 00:05:03 +0530

    Update IR.v

--- ANPPV_RISC_Pipelned_Processor.srcs/sources_1/imports/sources_1/new/IR.v
+++ ANPPV_RISC_Pipelned_Processor.srcs/sources_1/imports/sources_1/new/IR.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 
 module IR(
-    output reg  [7:0]   IROut,
-    input       [7:0]   memIR
+    output reg  [7:0]   IROut,  //holds the instruction opcode
+    input       [7:0]   memIR   //input from memory :: memory address taken from PC
     );
     initial 
         IROut = 8'h00;

202487ddbccb2934459a3a47a4b14d7bd51fc4c5
Paul George <command.paul@gmail.com>
2017-03-23 00:30:41 +0530

    Added Interface Comments

--- ANPPV_RISC_Pipelned_Processor.srcs/sources_1/imports/sources_1/imports/files/WBModule.v
+++ ANPPV_RISC_Pipelned_Processor.srcs/sources_1/imports/sources_1/imports/files/WBModule.v
@@ -1,11 +1,11 @@
 `timescale 1ns / 1ps
 
 module WBModule(
-    input DSP,
-    input [7:0] PCBuffer3,
-    input [7:0] Buffer3,
-    input       ERN,
-    output [7:0] WB
+    input         DSP,         // DSP Bit from CCG 4 :: Implies Stack Write
+    input   [7:0] PCBuffer3,   // The Value Held in the PC Buffer of the Stage 3-4 Buffer
+    input   [7:0] Buffer3,     // The Output of the ALU Stage
+    input         ERN,         // ERN CCG 4
+    output  [7:0] WB           // Output To 3 Port Memory Data Write Path
     );
     assign WB = (DSP&&~ERN)?PCBuffer3:Buffer3;
 endmodule

1b6a7b06e2dbc5e248563e34b5b99aeae8b1f7d1
AbhimanyuChoudhury <abhimanyu.choudhury@gmail.com>
2017-04-08 16:09:11 +0530

    Expansion of IR

--- Src/IR.v
+++ Src/IR.v
@@ -2,11 +2,11 @@
 
 module IR(
     output reg  [7:0]   IROut,  //holds the instruction opcode
-    input       [7:0]   memIR   //input from memory :: memory address taken from PC
+    input       [15:0]   memIR   //input from memory :: memory address taken from PC
     );
     initial 
         IROut = 8'h00;
         
-    always @(memIR) IROut = memIR;
+    always @(memIR) IROut = memIR[7:0]; 
     
 endmodule
